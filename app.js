5d2dde031dde0b2abb43d27061806341de7e9a13
Mohsina Mulani <mohsinamulani10@gmail.com>
2023-09-16 17:17:42 +0530

    Corrections

--- app.js
+++ app.js
@@ -1,60 +1,56 @@
-require('dotenv').config();
-const http = require('http');
-const express = require('express');
-const mongoose = require('mongoose');
-const Message = require('./src/models/registers')
+require("dotenv").config();
+const http = require("http");
+const express = require("express");
+const mongoose = require("mongoose");
+const Message = require("./src/models/registers");
 // const port = process.env.PORT || 3001;
 //express app
 // const app = express();
-// const express=require("express"); 
-const app=express();  
-const path=require("path");
+// const express=require("express");
+const app = express();
+const path = require("path");
 // require("./db/conn");
-const Register=require("./src/models/registers");
-const port=process.env.PORT || 3000; 
-console.log(path.join(__dirname,"public")); 
-const static_path=path.join(__dirname,"public");  
-app.use(express.json()); 
-app.use(express.static('public'));
-app.use(express.urlencoded({extended:true})); 
+const Register = require("./src/models/registers");
+const port = process.env.PORT || 5000;
+console.log(path.join(__dirname, "public"));
+const static_path = path.join(__dirname, "public");
+app.use(express.json());
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
-
-
-
-
-
-mongoose.connect( process.env.MONGODB_URL,{ useNewUrlParser:true , useUnifiedTopology:true})
+mongoose.connect(process.env.MONGODB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 try {
-    // Connect to the MongoDB cluster
-     mongoose.connect(
-      process.env.MONGODB_URL,
-      { useNewUrlParser: true, useUnifiedTopology: true },
-      () => console.log(" Mongoose is connected")
-    );
+  // Connect to the MongoDB cluster
+  mongoose.connect(
+    process.env.MONGODB_URL,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => console.log(" Mongoose is connected")
+  );
+} catch (e) {
+  console.log("could not connect");
+}
 
-  } catch (e) {
-    console.log("could not connect");
-  }
-
-app.get("/",(req,res)=> { 
-    res.send("Hellloooo");
-}); 
- app.get("/index",(req,res) => {
-    res.render("index");
- }); 
- app.post('/',function(req,res){
-    const msg = new Message(req.body);
-    msg.save()
-    .then((result) =>{
-        res.redirect('/success.html')
-        
+app.get("/", (req, res) => {
+  res.send("Hellloooo");
+});
+app.get("/index", (req, res) => {
+  res.render("index");
+});
+app.post("/", function (req, res) {
+  const msg = new Message(req.body);
+  msg
+    .save()
+    .then((result) => {
+      res.redirect("/success.html");
     })
-    .catch((err) =>{
-        console.log(err);
-        res.redirect('/error.html')
+    .catch((err) => {
+      console.log(err);
+      res.redirect("/error.html");
     });
-})
-app.listen(port,()=> { 
-    console.log(`server is running at port no ${port}`); 
-
-})
+});
+app.listen(port, () => {
+  console.log(`server is running at port no ${port}`);
+});
